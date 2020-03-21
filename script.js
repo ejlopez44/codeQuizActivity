990815971053192bb3a391631bbe32b0131f6895
Tito <ejlopez44@gmail.com>
2020-03-21 02:43:43 -0400

    added question objects to js file

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+//Question Objects
+var qOne = {
+    Question: "Who makes the best motorcycles",
+    choiceA: "USA",
+    choiceB: "Germany",
+    choiceC: "Italy",
+    choiceD: "Japan",
+    answer: "Italy",
+}
+var qTwo = {
+    Question: "Who is the greatest racer of all time?",
+    choiceA: "Marc Marquez",
+    choiceB: "Valentino Rossi",
+    choiceC: "Alvaro Bautista",
+    choiceD: "Randy Depuniet",
+    answer: "Valentino Rossi"
+}
+var qThree = {
+    Question: "Who's America's motorcycle brand?",
+    choiceA: "Ducati",
+    choiceB: "Yamaha",
+    choiceC: "Harley Davidson",
+    choiceD: "Ural",
+    answer: "Harley Davidson"
+}
+var qFour = {
+    Question: "Which of these brands doesn't run on marinara sauce?",
+    choiceA: "Ducati",
+    choiceB: "Bimota",
+    choiceC: "Aprilia",
+    choiceD: "Bajaj",
+    answer: "Bajaj"
+}
+
+
+
+
+
+
 var startQuizBtn = document.getElementById('startQuiz')
 
 // Quiz Timer related variables and functions
@@ -15,5 +54,15 @@ var quizTimer = function () {
         }
     }, 1000);
 }
+
+//question object calling and display function
+
+function presentQuestion(obj) {
+
+
+}
+
+
+
 
 startQuizBtn.addEventListener('click', quizTimer)
