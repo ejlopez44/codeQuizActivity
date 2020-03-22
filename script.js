c3385fa9fcb99b2d3e5268ac6a46d9bf274516fb
Tito <ejlopez44@gmail.com>
2020-03-22 18:11:29 -0400

    Reorganized question array, Created temporary Skip button

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
 //Question Objects
-var qOne = [
-    { Question: "Who makes the best motorcycles?" },
-    { choice: "USA", answer: false },
-    { choice: "Germany", answer: false },
-    { choice: "Italy", answer: true },
-    { choice: "Japan", answer: false },
-]
-var qTwo = [
-    { Question: "Who is the greatest racer of all time?" },
-    { choice: "Marc Marquez", answer: false },
-    { choice: "Valentino Rossi", answer: true },
-    { choice: "Alvaro Bautista", answer: false },
-    { choice: "Randy Depuniet", answer: false },
-]
-var qThree = [
-    { Question: "Who's America's motorcycle brand?" },
-    { choice: "Ducati", answer: false },
-    { choice: "Yamaha", answer: false },
-    { choice: "Harley Davidson", answer: true },
-    { choice: "Ural", answer: false },
-]
-var qFour = [
-    { Question: "Which of these brands doesn't run on marinara sauce?" },
-    { choice: "Ducati", answer: false },
-    { choice: "Bimota", answer: false },
-    { choice: "Aprilia", answer: false },
-    { choice: "Bajaj", answer: true },
-]
+
+var questionArray = [
+    // questionArray[0], questionArray[0][0].Question
+    [
+        { Question: "Who makes the best motorcycles?" },
+        { choice: "USA", answer: false },
+        { choice: "Germany", answer: false },
+        { choice: "Italy", answer: true },
+        { choice: "Japan", answer: false },
+    ], [
+
+        { Question: "Who is the greatest racer of all time?" },
+        { choice: "Marc Marquez", answer: false },
+        { choice: "Valentino Rossi", answer: true },
+        { choice: "Alvaro Bautista", answer: false },
+        { choice: "Randy Depuniet", answer: false },
+    ], [
+        { Question: "Who's America's motorcycle brand?" },
+        { choice: "Ducati", answer: false },
+        { choice: "Yamaha", answer: false },
+        { choice: "Harley Davidson", answer: true },
+        { choice: "Ural", answer: false },
+    ], [
+
+        { Question: "Which of these brands doesn't run on marinara sauce?" },
+        { choice: "Ducati", answer: false },
+        { choice: "Bimota", answer: false },
+        { choice: "Aprilia", answer: false },
+        { choice: "Bajaj", answer: true },
+    ]]
+
 
 //Globally scoped variables
 var questionBlk = document.getElementById('questionBlk')
 var startBtn = document.getElementById('startbtn')
-var currentScore = ""
+var questionIndex = 0;
+var currentScore = 0;
 var highScores = {}
 
 // Quiz Timer function
@@ -41,7 +45,7 @@ var quizTimer = function () {
     var secondsLeft = 100;
     var timerInterval = setInterval(function () {
         secondsLeft--;
-        countDown.textContent = secondsLeft;
+        countDown.textContent = "Timer: " + secondsLeft;
 
         if (secondsLeft === 0) {
             clearInterval(timerInterval)
@@ -50,55 +54,83 @@ var quizTimer = function () {
     }, 1000);
 }
 
-//Quiz Starting
+//Quiz Starting version 1
+// function startQuiz() {
+//     ///These two lines remove Start button and Description
+//     startBtn.style.display = "none";
+//     document.getElementById('quizDescription').style.display = "none";
+//     // This function calls the function that creates elements for Question and Button Choice elements
+//     presentQuestion(questionArray[0]);
+//     //This calls the timer function
+//     quizTimer();
+// }
+
+//Quiz Start version 2 with loop
+function nextQuestion() {
+    presentQuestion(questionArray[questionIndex]);
+
+    var skipBtn = document.createElement('button');
+    skipBtn.textContent = " Skip ";
+    document.querySelector('#sideRow').appendChild(skipBtn);
+    skipBtn.addEventListener('click', skipToNext)
+}
+
 function startQuiz() {
     ///These two lines remove Start button and Description
     startBtn.style.display = "none";
     document.getElementById('quizDescription').style.display = "none";
+    nextQuestion();
+    //foreach loop that will allow quiz to transition through each question, IF receives a button input...?
+
+
+
     // This function calls the function that creates elements for Question and Button Choice elements
-    presentQuestion(qOne);
+
+    //This calls the timer function
+    quizTimer();
 }
 
-//This check function checks if the button choice selected is true or false and calls
-
-function check() {
-
+function reset() {
+    var btnBlock = document.getElementById('btnBlock')
+    while (btnBlock.hasChildNodes()) {
+        btnBlock.removeChild(btnBlock.firstChild);
+    }
 }
+
+
 
 // This block of code updates the question/heading block & takes in the Object names and creates buttons foreach choice...
-// This could be made into a For loop that creates each button element for each choice within the object. May need to change layout of the object..
+
 function presentQuestion(question) {
     questionBlk.innerText = question[0].Question
-
+    questionIndex++;
+    // this loops through the remaining objects of the question arrary and creates a button for each one with it's string
     for (let i = 1; i < question.length; i++) {
         const element = question[i];
         var choiceBtn = document.createElement('button');
+        // This line applies the string of text to the button
         choiceBtn.textContent = question[i].choice
+        // This line is necessary to call if a button has a dataset entry
+        choiceBtn.classList.add('btnVal')
+        //This line creates the button element
         document.getElementById('btnBlock').appendChild(choiceBtn)
-
-
+        if (question[i].answer)
+            choiceBtn.dataset.answer = question[i].answer
     }
-    //previous way to generate
-    // var choiceBtn1 = document.createElement('button');
-    // choiceBtn1.textContent = question[1].choiceA
-    // document.getElementById('btnBlock').appendChild(choiceBtn1)
-
-    // var choiceBtn2 = document.createElement('button');
-    // choiceBtn2.textContent = question.choiceB
-    // document.getElementById('btnBlock').appendChild(choiceBtn2)
-
-
-    // var choiceBtn3 = document.createElement('button');
-    // choiceBtn3.textContent = question.choiceC
-    // document.getElementById('btnBlock').appendChild(choiceBtn3)
-
-
-    // var choiceBtn4 = document.createElement('button');
-    // choiceBtn4.textContent = question.choiceD
-    // document.getElementById('btnBlock').appendChild(choiceBtn4)
-
-
+    // !!!!!! THIS NEEDS TO BE CODED
+    //choiceBtn.addEventListener('click', checkChoice)
 }
+
+function skipToNext() {
+    reset()
+    presentQuestion(questionArray[questionIndex])
+}
+
+// function checkChoice(param) {
+//     if ()
+// }
+
+
 //  each button needs a function that... 
 //checks if true(correct) or false(wrong)
 // moves you to the next question (re calls function for the next Question) 
